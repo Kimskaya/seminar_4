66321f034f86de072bcfbb8303b9baaf9f4c5a88
AnnaKim <annakim@inbox.ru>
2023-01-14 05:37:18 +0300

    task_29

--- task_29/Program.cs
+++ task_29/Program.cs
@@ -2,32 +2,32 @@
 
 //1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 
-//6, 1, 33 -> [6, 1, 33]
+// 6, 1, 33 -> [6, 1, 33]
 
 int [] GetRandomArray (int Length)
 {
-    int [] arr = new int[Length];
-    for (int i = 0; i < Length; i++)
+    int [] arr = new int [Length];
+    for (int i = 0;i < Length; i ++)
     {
-        arr [i] = new Random().Next(0,10);
+         arr [i] = new Random().Next();
     }
     return arr;
 }
-int [] Array = GetRandomArray(8);
- 
-void ArrayOutput (int Array)
+int [] Array = GetRandomArray (8);
+
+void ArrayOutput (int [] Array)
 {
-    Console.Write($"[");
-    for (int i = 0; i<Array.Length; i ++)
+    Console.Write ($"[");
+    for(int i = 0; i <Array.Length; i ++)
     {
-        if (i != Array.Length- 1)
+        if (i != Array.Length-1)
         {
             Console.Write($"{Array[i]}, ");
         }
         else
         {
-            Console.Write($"{Array[i]}");
+            Console.Write($"{Array[i]}]");
         }
     }
-} 
+}
 ArrayOutput (Array);
